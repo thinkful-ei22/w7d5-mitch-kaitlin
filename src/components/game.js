7be12447bf4305331975199a84104dc3c9987b15
MitchGianoni <mitchgianoni@yahoo.com>
2018-07-26 11:50:32 -0400

    started on infomodal

--- src/components/game.js
+++ src/components/game.js
@@ -5,14 +5,27 @@ import GuessSection from './guess-section';
 import GuessCount  from './guess-count';
 import GuessList from './guess-list';
 
-export default function Game(props) {
-  return (
-    <div>
-      <Header />
-      <GuessSection feedback="Make your guess!" />
-      <GuessCount count={3} />
-      <GuessList guesses={[10, 15, 25]} />
-    </div>
-  );
+export default class Game extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showInfo: false
+    };
+  }
+
+  toggleInfo() {
+    const showInfo = this.state.showInfo;
+    this.setState({showInfo: !showInfo});
+  }
+
+  render(){
+    return (
+      <div>
+        <Header showInfo={this.state.showInfo} />
+        <GuessSection feedback="Make your guess!" />
+        <GuessCount count={3} />
+        <GuessList guesses={[10, 15, 25]} />
+      </div>
+  );}
 }
 
